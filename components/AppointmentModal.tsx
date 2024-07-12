615f04411a18a0ed9cec633f7e0680f86ef74e4d
Justin24506 <justatwork65@outlook.com>
2024-07-12 16:41:10 +0530

    appointment modal test

--- components/AppointmentModal.tsx
+++ components/AppointmentModal.tsx
@@ -17,34 +17,15 @@ const AppointmentModal = ({ type, patientId, userId, appointment }: {
   type: 'schedule' | 'cancel';
   patientId: string;
   userId: string;
-  appointment: Appointment;
+  appointment?: Appointment;
 }) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger>
-        <Button asChild variant="ghost" className={`capitalize ${type === 'schedule' && 'text-green-500'}`}>
-          {type}
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="shad-dialog sm:max-w-md">
-        <DialogHeader className="mb-4 space-y-3">
-          <DialogTitle className="capitalize">{type} this Appointment</DialogTitle>
-          <DialogDescription>
-            Please fill out the form below to {type} an appointment.
-          </DialogDescription>
-        </DialogHeader>
-
-        <AppointmentForm 
-          type={type}
-          patientId={patientId}
-          userId={userId}
-          appointment={appointment}
-          setOpen={setOpen}
-          />
-      </DialogContent>
-    </Dialog> 
+    <Button asChild variant="ghost" className={`capitalize ${type === 'schedule' && 'text-green-500'}`}>
+      {type}
+    </Button>
+    
     
 
   )
